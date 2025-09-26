085fd71a38c690e57c9c52ba82fff98f82a494f5
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-09-26 14:21:10 +0200

    Dino_MVC_CQS/Create project first commit

---
+++ Dino_MVC_CQS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dino_MVC_CQS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
